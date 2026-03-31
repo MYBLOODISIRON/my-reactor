c7295682936ba5cdf06fcdd42be3e65307435664
Killy <3335655852@qq.com>
2026-03-31 22:15:31 +0800

    添加封装网络地址类InetAddress

---
+++ InetAddress.cc
@@ -0,0 +1,34 @@
+#include "InetAddress.h"
+#include <strings.h>
+#include <string.h>
+
+InetAddress::InetAddress(uint16_t port, const std::string& ip = "127.0.0.1")
+{
+    bzero(&m_addr, sizeof(m_addr));
+    m_addr.sin_family = AF_INET;
+    m_addr.sin_port = htons(port);
+    m_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+}
+
+std::string InetAddress::toIp() const
+{
+    char buffer[64] {0};
+    inet_ntop(AF_INET, &m_addr.sin_addr, buffer, 64);
+    return buffer;
+}
+
+std::string InetAddress::toIpPort() const
+{
+    char buffer[64] {0};
+    inet_ntop(AF_INET, &m_addr.sin_addr, buffer, 64);
+    size_t length {strlen(buffer)};
+    uint16_t port {ntohs(m_addr.sin_port)};
+    sprintf(buffer + length, ":%u", port);
+    return buffer;
+}
+
+
+uint16_t InetAddress::toPort() const
+{
+    return ntohs(m_addr.sin_port);
+}
---
+++ InetAddress.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <arpa/inet.h>
+
+
+class InetAddress
+{
+    private:
+        struct sockaddr_in m_addr;
+    public:
+        explicit InetAddress(uint16_t port, const std::string& ip);
+        explicit InetAddress(const sockaddr_in& addr);
+        std::string toIp() const;
+        std::string toIpPort() const;
+        uint16_t toPort() const;
+        const sockaddr_in* getSockAddr() const;
+};
