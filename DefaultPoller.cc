b1a3e69a28b2cd254ec26c9e7d8d611daf13cbc3
Killy <3335655852@qq.com>
2026-04-02 01:18:22 +0800

    添加Poller类

---
+++ DefaultPoller.cc
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+#include "Poller.h"
+
+Poller* Poller::newDefaultPoller(EventLoop *loop)   // 可能需要包含子类的头文件，所以不放在Poller.cc中
+{
+    if(getenv("MYREACTOR_USE_POLL"))
+    {
+        return nullptr;
+    }
+    else
+    {
+        return nullptr;
+    }
+}
---
+++ Poller.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+
+#include "noncopyable.h"
+
+class Channel;
+
+
+
+class Poller: noncopyable
+{
+    private:
+        EventLoop*  m_ownerLoop;
+    protected:
+        using ChannelMap = std::unordered_map<int, Channel*>;
+        ChannelMap  m_channels;
+
+
+    public:
+        using ChannelList = std::vector<Channel*>;
+
+        Poller  (EventLoop *loop);
+        virtual ~Poller ();
+
+        static  Poller*     newDefaultPoller(EventLoop *loop);   // 可能需要包含子类的头文件，所以不放在Poller.cc中
+
+        virtual Timestamp   poll    (int timeoutms, ChannelList* activeChannels) = 0;
+        virtual void        updateChannel   (Channel *channel) = 0;
+        virtual void        removeChannel   (Channel *channel) = 0;
+
+        bool    hasChannel  (Channel *channel) const;
+};
