5d96a769585db7c3ce54a24cd7a41523172cb570
Killy <3335655852@qq.com>
2026-04-03 14:38:05 +0800

    添加CurrentThread辅助函数

---
+++ CurrentThread.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+namespace CurrentThread
+{
+    extern __thread int t_cachedTid;
+
+    void cachedTid();
+
+    inline int tid()
+    {
+        if(__builtin_expect(t_cachedTid == 0, 0))
+        {
+            cachedTid();
+        }
+
+        return t_cachedTid;
+    }
+}
