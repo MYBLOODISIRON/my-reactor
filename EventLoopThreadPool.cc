a1fbf0e74c16cb7cfa33b6a564434b9520f08295
Killy <3335655852@qq.com>
2026-04-03 23:04:01 +0800

    添加并实现EventLoopThreadPool类

---
+++ EventLoopThreadPool.cc
@@ -0,0 +1,87 @@
+
+#include <memory>
+
+#include "EventLoopThreadPool.h"
+#include "EventLoopThread.h"
+
+
+EventLoopThreadPool::EventLoopThreadPool(EventLoop *baseLoop, const std::string &nameArg)
+:   m_baseLoop  {baseLoop},
+    m_name      {nameArg}
+{
+
+}
+
+
+EventLoopThreadPool::~EventLoopThreadPool()
+{
+
+}
+
+
+void EventLoopThreadPool::start(const ThreadInitCallback &cb)
+{
+    m_started = true;
+    
+    for(int i = 0; i < m_numThreads; i ++)
+    {
+        const size_t buf_len {m_name.size() + 32};
+        char buffer[buf_len];
+        snprintf(buffer, sizeof buffer, "%s%d", m_name.c_str(), i);
+        EventLoopThread *t = new EventLoopThread {cb, buffer};
+        m_threads.push_back(std::make_unique<EventLoopThread>(t));
+        m_loops.push_back(t->startLoop());
+    }
+
+    if(m_numThreads == 0 && cb)
+    {
+        cb(m_baseLoop);
+    }
+}
+
+
+EventLoop* EventLoopThreadPool::getNextLoop()
+{
+    EventLoop *loop = m_baseLoop;
+    if(! m_loops.empty())
+    {
+        loop = m_loops[m_next];
+        m_next ++;
+        if(m_next >= m_loops.size())
+        {
+            m_next = 0;
+        }
+    }
+    return loop;
+}
+
+
+std::vector<EventLoop*> EventLoopThreadPool::getAllLoops() const
+{
+    if(m_loops.empty())
+    {
+        return std::vector<EventLoop*> {1, m_baseLoop};
+    }
+    else
+    {
+        return m_loops;
+    }
+}
+
+
+void EventLoopThreadPool::setNumThreads(int numThreads)
+{
+    m_numThreads = numThreads;
+}
+
+
+bool EventLoopThreadPool::started() const
+{
+    return m_started;
+}
+
+
+const std::string& EventLoopThreadPool::name() const
+{
+    return m_name;
+}
---
+++ EventLoopThreadPool.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "noncopyable.h"
+
+class EventLoopThread;
+class EventLoop;
+
+class EventLoopThreadPool: noncopyable
+{
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    private:
+
+        EventLoop   *m_baseLoop {nullptr};
+        std::string m_name;
+        bool        m_started   {false};
+        int         m_numThreads    {0};
+        int         m_next          {0};
+        std::vector< std::unique_ptr< EventLoopThread > >   m_threads;
+        std::vector< EventLoop* >                           m_loops;
+
+    public:
+
+        EventLoopThreadPool (EventLoop *baseLoop, const std::string &nameArg);
+        ~EventLoopThreadPool    () = default;   // EventLoop全都是线程栈上的变量
+
+        void    setNumThreads   (int numThreads);
+        void    start(const ThreadInitCallback &cb = ThreadInitCallback());
+        EventLoop*  getNextLoop (); // 多线程中，baseLoop默认以轮询分配channel给subloop
+        std::vector< EventLoop* >   getAllLoops ()  const;
+
+        bool    started () const;
+        const std::string&  name    () const;
+};
