b210dd38155ed40e26cc8f360a838cbbe5f36ef3
Killy <3335655852@qq.com>
2026-04-03 17:19:46 +0800

    添加并实现EventLoop类

---
+++ EventLoop.cc
@@ -0,0 +1,183 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/eventfd.h>
+#include <memory>
+#include <mutex>
+#include <vector>
+
+
+#include "Channel.h"
+#include "EventLoop.h"
+#include "Logger.h"
+#include "CurrentThread.h"
+#include "Poller.h"
+
+__thread EventLoop *t_loopInThread = nullptr; // 防止一个线程创建多个EventLoop
+
+const int kPollTimeMs = 10000; // 默认超时时间
+
+int createEventfd()
+{
+    int evfd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if(evfd < 0)
+    {
+        LOG_FATAL("evfd error: %d\n", errno);
+    }
+
+    return evfd;
+}
+
+
+
+EventLoop::EventLoop()
+: m_looping {false}, m_quit {false}, m_callingPendingFunctor {false}, m_threadId {CurrentThread::tid()}, m_poller {Poller::newDefaultPoller(this)}, m_wakeupFd {createEventfd()}, m_wakeupChannel {new Channel {this, m_wakeupFd}}, m_currentActiveChannel {nullptr}
+{
+    if(t_loopInThread)
+    {
+        LOG_FATAL("Another EventLoop %p is exist in this thread %d\n", t_loopInThread, m_threadId);
+    }
+    else
+    {
+        t_loopInThread = this;
+    }
+
+    // 设置wakeupfd的事件类型及其发生后的回调
+    m_wakeupChannel->setReadCallback(std::bind(&EventLoop::handleRead, this));
+    m_wakeupChannel->enableReading();
+}
+
+EventLoop::~EventLoop()
+{
+    m_wakeupChannel->disableAll();
+    m_wakeupChannel->remove();
+    close(m_wakeupFd);
+    t_loopInThread = nullptr;
+}
+
+
+void EventLoop::handleRead()
+{
+    uint64_t one = 1;
+    ssize_t n = read(m_wakeupFd, &one, sizeof(one));
+    if(n != 64)
+    {
+        LOG_ERROR("EventLoop::handleRead() reads %d bytes instead of 8\n", n);
+    }
+}
+
+
+void EventLoop::loop()
+{
+    m_looping = true;
+    m_quit = false;
+
+    while(! m_quit)
+    {
+        m_activeChannels.clear();
+        m_pollReturnTime = m_poller->poll(kPollTimeMs, &m_activeChannels);
+        for(Channel* channel : m_activeChannels)
+        {
+            channel->handleEvent(m_pollReturnTime);
+        }
+
+        doPendingFunctor(); // 执行EventLoop事件循环需要处理的回调操作,由mainloop注册，subloop执行
+    }
+    m_looping = false;
+}
+
+
+void EventLoop::quit()
+{
+    m_quit = true;
+
+    if(! isInLoopThread())  // 非loop所在的线程调用quit，会唤醒loop所在的线程，并且跳出while循环
+    {
+        wakeup();
+    }
+}
+
+
+void EventLoop::runInLoop(Functor cb)
+{
+    if(isInLoopThread())
+    {
+        cb();
+    }
+    else    // 非loop所在的线程
+    {
+        queueInLoop(cb);
+    }
+}
+
+
+void EventLoop::queueInLoop(Functor cb)
+{
+    {
+        std::unique_lock<std::mutex>    lock    {m_mutex};
+        m_pendingFunctors.emplace_back(cb);
+
+    }
+
+    if(! isInLoopThread() || m_callingPendingFunctor)  // 唤醒loop所在的线程
+    {
+        wakeup();
+    }
+}
+
+
+void EventLoop::wakeup()
+{
+    uint64_t one {0};
+    ssize_t n {write(m_wakeupFd, &one, 8)};
+    if(n != 8)
+    {
+        LOG_ERROR("EventLoop::wakeup() writes %lu bytes instead of 8 bytes.\n", n);
+    }
+}
+
+
+void EventLoop::updateChannel(Channel *channel)
+{
+    m_poller->updateChannel(channel);
+}
+
+
+void EventLoop::removeChannel(Channel *channel)
+{
+    m_poller->removeChannel(channel);
+}
+
+bool EventLoop::hasChannel(Channel *channel)
+{
+    return m_poller->hasChannel(channel);
+}
+
+
+void EventLoop::doPendingFunctor()
+{
+    std::vector<Functor>    functors;
+    m_callingPendingFunctor = true;
+
+    {
+        std::unique_lock<std::mutex>    lock    {m_mutex};
+        functors.swap(m_pendingFunctors);   // 可以和注册回调同时执行
+    }
+
+    for(const auto& functor : functors)
+    {
+        functor();  // 执行回调
+    }
+
+    m_callingPendingFunctor = false;
+}
+
+
+bool EventLoop::isInLoopThread() const
+{
+    return m_threadId == CurrentThread::tid();
+}
+
+Timestamp EventLoop::pollReturnTime() const
+{
+    return m_pollReturnTime;
+}
---
+++ EventLoop.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+#include <atomic>
+#include <memory>
+#include <mutex>
+
+#include "noncopyable.h"
+#include "Timestamp.h"
+
+class Channel;
+class Poller;
+
+
+class EventLoop
+{
+    using Functor = std::function<void()>;
+    using ChannelList = std::vector<Channel*>;
+
+    private:
+
+        std::atomic_bool    m_looping;
+        std::atomic_bool    m_quit;     // 标志推出循环
+        const   pid_t       m_threadId;     // loop所在的线程
+        Timestamp           m_pollReturnTime;
+        std::unique_ptr<Poller> m_poller;
+
+        int m_wakeupFd; // mainloop获取新Channel，唤醒subloop，是一个eventfd
+        std::unique_ptr<Channel>    m_wakeupChannel;
+
+        ChannelList m_activeChannels;
+        Channel*    m_currentActiveChannel;
+
+        std::atomic_bool        m_callingPendingFunctor;    // 标识当前loop是否有需要执行的回调
+        std::vector<Functor>    m_pendingFunctors;
+        std::mutex              m_mutex;    // 保护m_pendingFunctors操作
+
+    public:
+        EventLoop   ();
+        ~EventLoop  ();
+
+        void loop   (); // 开启事件循环
+        void quit   (); // 退出事件循环
+
+        Timestamp   pollReturnTime  () const;
+
+        void    runInLoop   (Functor cb);   // 当前loop执行cb
+        void    queueInLoop (Functor cb);   // cb放进queue，唤醒loop所在的线程，执行cb
+
+        void    wakeup  ();
+        void    updateChannel   (Channel *channel);
+        void    removeChannel   (Channel *channel);
+        bool    hasChannel      (Channel *channel);
+
+        bool     isInLoopThread () const;
+    
+    private:
+
+        void handleRead ();
+        void doPendingFunctor   ();
+};
