8ff67d633e64c50864ba5aedf64b79f3e431e5f1
Killy <3335655852@qq.com>
2026-04-04 16:50:39 +0800

    添加并实现包装原生linux socket的Socket类

---
+++ Socket.cc
@@ -0,0 +1,95 @@
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <string.h>
+
+#include "Socket.h"
+#include "Logger.h"
+#include "InetAddress.h"
+
+
+Socket::Socket(int sockfd)
+:   m_sockfd    {sockfd}
+{
+
+}
+
+
+Socket::~Socket()
+{
+    close(m_sockfd);
+}
+
+int Socket::fd() const
+{
+    return m_sockfd;
+}
+
+void Socket::bindAddress(const InetAddress& localAddr)
+{
+    if(0 != bind(m_sockfd, (sockaddr*)localAddr.getSockAddr(), sizeof(sockaddr_in)))
+    {
+        LOG_FATAL("bind sockfd: %d failed.\n", m_sockfd);
+    }
+}
+
+void Socket::listen()
+{
+    if(0 != ::listen(m_sockfd, 1024))
+    {
+        LOG_FATAL("listen sockfd: %d failed.\n", m_sockfd);
+    }
+}
+
+
+int Socket::accept(InetAddress* peerAddr)
+{
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof addr);
+    socklen_t len {sizeof addr};
+
+    int connfd = ::accept(m_sockfd, (sockaddr*) &addr, &len);
+    if(connfd >= 0)
+    {
+        peerAddr->setSockAddr(addr);
+    }
+    return connfd;
+}
+
+
+void Socket::shutdownWrite()
+{
+    if(::shutdown(m_sockfd, SHUT_WR) < 0)
+    {
+        LOG_ERROR("shutdown error.\n");
+    }
+}
+
+
+void Socket::setTcpNoDelay(bool on)
+{
+    int optval {on ? 1 : 0};
+    ::setsockopt(m_sockfd, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof optval);
+}
+
+
+void Socket::setResultAddr(bool on)
+{
+    int optval {on ? 1 : 0};
+    ::setsockopt(m_sockfd, SQL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+}
+
+void Socket::setReusePort(bool on)
+{
+    int optval {on ? 1 : 0};
+
+    ::setsockopt(m_sockfd, SQL_SOCKET, SO_REUSEPORT, &optval, sizeof optval);
+}
+
+void Socket::setKeepAlive(bool on)
+{
+    int optval {on ? 1 : 0};
+    ::setsockopt(m_sockfd, SQL_SOCKET, SO_KEEPALIVE, &optval, sizeof optval);
+}
---
+++ Socket.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+#include "noncopyable.h"
+
+class InetAddress;
+
+class Socket
+{
+    private:
+
+        const int m_sockfd;
+
+    public:
+
+        explicit Socket (int sockfd);
+        ~Socket         ();
+
+        int fd  () const;
+        void    bindAddress (const InetAddress& localAddr);
+        void    listen      ();
+        int     accept      (InetAddress *peerAddr);
+        void    shutdownWrite   ();
+
+        void    setTcpNoDelay   (bool on);
+        void    setResultAddr   (bool on);
+        void    setReusePort    (bool on);
+        void    setKeepAlive    (bool on);
+};
