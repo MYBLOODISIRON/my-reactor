deb292f904ee7603832044e3fb22db2f428900a0
Killy <3335655852@qq.com>
2026-04-05 14:16:40 +0800

    完善Buffer类

---
+++ Buffer.cc
@@ -0,0 +1,136 @@
+#include <sys/uio.h>
+#include <errno.h>
+#include <algorithm>
+#include "Buffer.h"
+
+Buffer::Buffer(size_t initialSize)
+:   m_buffer    {sm_kCheapPrepend + initialSize},
+    m_readIndex {sm_kCheapPrepend},
+    m_writeIndex    {sm_kCheapPrepend}
+{
+
+}
+
+size_t Buffer::readableBytes() const
+{
+    return m_writeIndex - m_readIndex;
+}
+
+size_t Buffer::writableBytes() const
+{
+    return m_buffer.size() - m_writeIndex;
+}
+
+size_t Buffer::prependableBytes() const
+{
+    return m_readIndex;
+}
+
+const char* Buffer::begin()
+{
+    return m_buffer.data();
+}
+
+
+const char* Buffer::peek() const
+{
+    return begin() + m_readIndex;
+}
+
+
+void Buffer::retrieve(size_t len)
+{
+    if(len < readableBytes())
+    {
+        m_readIndex += len;
+    }
+    else
+    {
+        retrieveAll();
+    }
+}
+
+void Buffer::retrieveAll()
+{
+    m_readIndex = m_writeIndex = sm_kCheapPrepend;
+}
+
+std::string retrieveAllAsString()
+{
+    return retrieveAsString(readableBytes());
+}
+
+std::string retrieveAsString(size_t len)
+{
+    std::string result {peek(), len};
+    retrieve(len);
+    return result;
+}
+
+void Buffer::ensureWritableBytes(size_t len)
+{
+    if(writableBytes() < len)
+    {
+        makeSpace(len);
+    }
+}
+
+void Buffer::makeSpace(size_t len)
+{
+    if(readableBytes() + prependableBytes() < len + sm_kCheapPrepend)
+    {
+        m_buffer.resize(m_writeIndex + len);
+    }
+    else
+    {
+        size_t readable = readableBytes();
+        std::copy(begin() + m_readIndex, begin()+ m_writeIndex, begin() + sm_kCheapPrepend);
+        m_readIndex = sm_kCheapPrepend;
+        m_writeIndex = m_readIndex + readable;
+    }
+}
+
+char* Buffer::beginWrite()
+{
+    return begin() + m_writeIndex;
+}
+
+const char* Buffer::beginWrite() const
+{
+    return begin() + m_writeIndex;
+}
+
+void Buffer::append(const char* data, size_t len)
+{
+    ensureWritableBytes(len);
+    std::copy(data, data + len, beginWrite());
+    m_writeIndex += len;
+}
+
+ssize_t Buffer::readFd(int fd, int* saveErrno)
+{
+    char extrabuf [65535];
+    struct iovec vec [2];
+    const size_t writable = writableBytes();
+    vec[0].iov_base = begin() + m_writeIndex;
+    vec[1].iov_base = extrabuf;
+    vec[0].iov_len = writable;
+    vec[1].iov_len = sizeof extrabuf;
+
+    const int iov_count = (writable < sizeof extrabuf) ? 2 : 1;
+    const ssize_t n = ::readv(fd, vec, iov_count);
+    if(n < 0)
+    {
+        *saveErrno = errno;
+    }
+    else if (n <= writable)
+    {
+        m_writeIndex += n;
+    }
+    else
+    {
+        m_writeIndex = m_buffer.size();
+        append(extrabuf, n - writable);
+    }
+    return n; 
+}
--- Buffer.h
+++ Buffer.h
@@ -1,7 +1,35 @@
 #pragma once
+#include <vector>
+#include <string>
 
-class Buffer
+class Buffer    // 网络缓冲区
 {
-    private:
     public:
+        static const size_t sm_kCheapPrepend    {8};
+        static const size_t sm_kInitialSize     {1024};
+    private:
+        std::vector< char > m_buffer;
+        size_t              m_readIndex;
+        size_t              m_writeIndex;
+    public:
+
+        explicit    Buffer  (size_t initialSize = sm_kInitialSize);
+        ~Buffer () = default;
+
+        size_t  readableBytes   () const;
+        size_t  writableBytes   () const;
+        size_t  prependableBytes    () const;
+        const char* peek            () const;
+        void        retrieve         (size_t len);
+        void        retrieveAll      ();
+        std::string retrieveAllAsString ();
+        std::string retrieveAsString    (size_t len);
+        void        ensureWritableBytes (size_t len);
+        void        append              (const char* data, size_t len);
+        char*       beginWrite          ();
+        const char* beginWrite          () const;
+        ssize_t     readFd              (int fd, int* saveErrno);   // 从fd上读取数据
+    private:
+        const char* begin   ();
+        void        makeSpace   (size_t len);
 };
