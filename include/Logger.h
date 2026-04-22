1aafe674b486d70feee674bc86b54c1df6dc2c53
Killy <3335655852@qq.com>
2026-04-22 22:50:41 +0800

    修复Logger宏定义不能在同一作用域使用多次

--- include/Logger.h
+++ include/Logger.h
@@ -3,34 +3,34 @@
 #include "noncopyable.h"
 
 #define LOG_INFO(LogmsgFormat, ...)         \
-    Logger& logger {Logger::instance()};    \
+    {Logger& logger {Logger::instance()};    \
     logger.setLogLevel(INFO);               \
     char buffer[1024];                      \
     snprintf(buffer, 1024, LogmsgFormat, ##__VA_ARGS__);\
-    logger.log(buffer);
+    logger.log(buffer);}
 
 #define LOG_ERROR(LogmsgFormat, ...)         \
-    Logger& logger {Logger::instance()};    \
+    {Logger& logger {Logger::instance()};    \
     logger.setLogLevel(ERROR);               \
     char buffer[1024];                      \
     snprintf(buffer, 1024, LogmsgFormat, ##__VA_ARGS__);\
-    logger.log(buffer);
+    logger.log(buffer);}
 
 #define LOG_FATAL(LogmsgFormat, ...)         \
-    Logger& logger {Logger::instance()};    \
+    {Logger& logger {Logger::instance()};    \
     logger.setLogLevel(FATAL);               \
     char buffer[1024];                      \
     snprintf(buffer, 1024, LogmsgFormat, ##__VA_ARGS__);\
     logger.log(buffer);    \
-    exit(-1);
+    exit(-1);}
 
 #ifdef DEBUG_MOD
 #define LOG_DEBUG(LogmsgFormat, ...)         \
-    Logger& logger {Logger::instance()};    \
+    {Logger& logger {Logger::instance()};    \
     logger.setLogLevel(DEBUG);               \
     char buffer[1024];                      \
     snprintf(buffer, 1024, LogmsgFormat, ##__VA_ARGS__);\
-    logger.log(buffer);
+    logger.log(buffer);}
 #endif
 
 enum LogLevel
